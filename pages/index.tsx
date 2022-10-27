8938e266a1cfe2206df4b75c43823f732369f011
Your Namesanyamjain04 <sanyamjainsj04@gmail.com>
2022-10-27 18:24:47 +0530

    added widget in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,50 @@ import Login from "../components/Login";
 import Modal from "../components/Modal";
 import { modalState } from "../atoms/modalAtom";
 import { useRecoilState } from "recoil";
+import Widgets from "../components/Widgets";
 
-const Home: NextPage = ({ trendingResults, followResults, providers }: any) => {
+const Home: NextPage = ({ providers }: any) => {
+  
   const { data: session } = useSession();
   const [isOpen, setIsOpen] = useRecoilState(modalState);
+  const trendingResults = [
+    {
+      heading: "T20 World Cup 2021 · LIVE",
+      description:
+        "NZvAUS: New Zealand and Australia clash in the T20 World Cup final",
+      img: "https://rb.gy/d9yjtu",
+      tags: ["#T20WorldCupFinal, ", "Kane Williamson"],
+    },
+    {
+      heading: "Trending in United Arab Emirates",
+      description: "#earthquake",
+      img: "https://rb.gy/jvuy4v",
+      tags: ["#DubaiAirshow, ", "#gessdubai"],
+    },
+    {
+      heading: "Trending in Digital Creators",
+      description: "tubbo and quackity",
+      img: "",
+      tags: ["QUACKITY AND TUBBO,"],
+    },
+  ];
+  const followResults = [
+    {
+      userImg: "https://rb.gy/urakiy",
+      username: "SpaceX",
+      tag: "@SpaceX",
+    },
+    {
+      userImg: "https://rb.gy/aluxgh",
+      username: "Elon Musk",
+      tag: "@elonmusk",
+    },
+    {
+      userImg: "https://rb.gy/zyvazm",
+      username: "Tesla",
+      tag: "@Tesla",
+    },
+  ];
   if (!session) {
     return (
       <>
@@ -28,7 +68,7 @@ const Home: NextPage = ({ trendingResults, followResults, providers }: any) => {
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto ">
         <Sidebar />
         <Feed />
-        {/* Widgets */}
+        <Widgets followResults={followResults} trendingResults={trendingResults} />
 
         {isOpen && <Modal />}
       </main>
